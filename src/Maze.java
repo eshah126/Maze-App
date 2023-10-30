4ca5fb1ff82c5a8adb34cc8bc05bbd909c8971d6
Will Stenzel <wstenzel@stu.naperville203.org>
2023-10-30 13:48:05 -0500

    Maze skeleton done

---
+++ src/Maze.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Maze {
+    private Square[][] maze;
+    public Maze()
+    {
+    }
+
+    public boolean loadMaze(String fName)
+    {
+	
+        
+	    return true;
+    }
+    
+    public ArrayList<Square> getNeighbors(Square sq)
+    {
+	    
+        return neighbors;
+    }
+
+    public Square getStart()
+    {
+
+    }
+
+    public Square getFinish()
+    {
+
+    }
+
+    public void Reset()
+    [
+        
+    ]
+
+    public String toString()
+    {
+	
+    }
+
+}
---
+++ src/Square.java
@@ -0,0 +1,58 @@
+public class Square
+{
+    private int Row;
+    private int Col;
+    private int Type;
+    private final int TYPE;
+    public Square(int row, int col, int type)
+    {
+        this.Row=row;
+        this.Col=col;
+        this.Type=type;
+        this.TYPE = type;
+    }
+
+    public int getRow()
+    {
+        return this.Row;
+    }
+    public int getCol()
+    {
+        return this.Col;
+    }
+    public int getType()
+    {
+        return this.Type;
+    }
+
+    public void reset()
+    {
+        this.Type = this.TYPE;
+    }
+
+    public String toString()
+    {
+        int key = this.Type;
+        String out = null;
+        out = getRow()+" "+getCol()+" "+getType()+" ";
+        switch (key) {
+            case 0:
+                out += "_";
+                break;
+            case 1:
+                out += "#";
+                break;
+            case 2:
+                out += "S";
+                break;
+            case 3:
+                out += "E";
+                break;
+            default:
+                out += "ERROR";
+                break;
+        }
+        
+        return out;
+    }
+}
